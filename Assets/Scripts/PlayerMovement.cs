347e1130d162e658e2925ed11026854c6a3685d3
pankeDev <ngessner04@gmail.com>
2024-10-02 14:52:16 -0400

    Bat Movement Added

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField] private float moveSpeed;
+    private Rigidbody rb;
+    // don't need move y for this game.
+    private float moveX;
+    
+
+    void Start()
+    {
+        // reference rb
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+        movePlayer();
+    }
+
+    void movePlayer()
+    {
+        // get axis raw returns either 1 or 0 from the input systems horizontal logic,
+        // and is stored in moveX
+        moveX = Input.GetAxisRaw("Horizontal");
+
+        // move at input systems directed moveX, multiplied by moveSpeed. Keep the normal y velocity position (whatever its at basically)
+        rb.velocity = new Vector2(moveSpeed * moveX, rb.velocity.y);
+    }
+}
+
+
+// we want to move left, and right. 
+// We want that controlled by a set movespeed 
